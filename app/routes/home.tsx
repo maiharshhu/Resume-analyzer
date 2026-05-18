f5126fbe6046002de04f3dfa987d2112db08d4b9
Maiharshhu <hj.harsh440@gmail.com>
2026-05-18 12:21:09 +0530

    Authentication

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/component/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/constants";
+import { resume } from "react-dom/server";
+import ResumeCard from "~/component/ResumeCard";
+import { usePuterStore } from "~/lib/puter";
 
-export function meta({}: Route.MetaArgs) {
+
+export function meta({ }: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  const {auth} = usePuterStore();
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Application & Resume Ratings</h1>
+        <h2>Review your submission and check AI-Powered feedback.</h2>
+      </div>
+
+      {resume.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+
+        </div>
+      )}
+
+    </section>
+  </main>;
 }
